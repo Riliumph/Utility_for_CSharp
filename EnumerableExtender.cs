c73823b03fffe49c6a211d5801f5dd4c50ff8fa4
Riliumph <riliumph@gmail.com>
2016-07-06 22:54:22 +0900

    [Add]

---
+++ EnumerableExtender.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Extender
+{
+	/// <summary>
+	/// IEnumerableの拡張メソッド群
+	/// </summary>
+	public static class EnumerableExtender
+	{
+		public static T Find<T>( this IEnumerable<T> instance, Func<T, bool> predicate )
+		{
+			foreach ( var current in instance ) {
+				if ( predicate( current ) ) {
+					return current;
+				}
+			}
+			return default( T );
+		}
+
+		/// <summary>
+		/// IEnumerableが空もしくはnullである状態を検知します。
+		/// </summary>
+		/// <typeparam name="T">IEnumerable type</typeparam>
+		/// <param name="instance">チェック対象</param>
+		/// <returns>
+		///		<c>true</c> チェック対象がnull / empty
+		///		<c>false</c>チェック対象がそれ以外
+		/// </returns>
+		public static bool IsNullOrEmpty<T>( this IEnumerable<T> instance )
+		{
+			if ( instance == null ) {
+				return true;
+			}
+			// IEnumerable.Count() => O(n) / ICollection.Count =>O(1)
+			var collection = instance as ICollection<T>;
+			if ( collection != null ) {
+				return collection.Count < 1;
+			}
+			return !instance.Any();
+		}
+	}
+}
---
+++ StringExternder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extender
+{
+	/// <summary>
+	/// String系の拡張メソッド群
+	/// </summary>
+	public static class StringExtender
+	{
+		/// <summary>
+		/// 負値も取ることができるSubstring拡張メソッド
+		/// PythonのSubstring的なやつを参考にしてます。
+		/// </summary>
+		/// <param name="instance">切り取り対象</param>
+		/// <param name="begin">開始位置※負の場合、末尾から</param>
+		/// <param name="end">終了位置※負の場合、末尾から</param>
+		/// <returns></returns>
+		public static string Substr( this string instance, int begin, int end )
+		{
+			int bgn_idx = begin < 0 ? instance.Length + begin : begin;
+			int end_idx = end < 0 ? instance.Length + end : end;
+			return instance.Substring( bgn_idx, end_idx );
+		}
+
+		/// <summary>
+		/// Join拡張メソッド。もうPythonのアレです。
+		/// 標準のJoinメソッドをオーバーロードしてるので、気を付けてください。
+		/// </summary>
+		/// <example>
+		/// <code>
+		/// var list = new List<string>(){"aaa","bbb","ccc"};
+		/// ",".Join( list ) => aaa,bbb,ccc 
+		/// </code>
+		/// </example>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="instance">連結子</param>
+		/// <param name="enumerable">連結されるインスタンス</param>
+		/// <returns></returns>
+		public static string Join<T>( this string instance, IEnumerable<T> enumerable )
+		{
+			return string.Join( instance, enumerable );
+		}
+
+		public static string JoinMonad<T>( this string instance, IEnumerable<T> enumerable )
+		{
+			try {
+				return string.Join( instance, enumerable );
+			} catch ( Exception ) {
+				return default( string );
+			}
+		}
+	}
+}
